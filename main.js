ced634e15b47c382a693840854958342a523fb74
josetutis <149108908+josetutis@users.noreply.github.com>
2024-03-15 11:04:32 +0000

    created a function that passes 1 test

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 //HELLO BOOTCAMPER, FANCY SEEING YOU HERE.
 //HOPE YOU CAN HELP ME WITH MY BIRTHDAY PARTY. I FORGOT HOW MANY FRIENDS I HAVE BUT I HAVE
 //A LIST WITH SOME NAMES. I NEED TO BUY A BIRTHDAY CAKE BUT NOT SURE HOW MANY PIECES I NEED.
-//I REMEMBER NONE OF MY FRIEND'S NAMES IS LONGER THAN 5 CHARACTERES.
+//I REMEMBER NONE OF MY FRIEND'S NAMES IS  not LONGER THAN 6 CHARACTERES.
 
-const friends = ["Juan", "Marcos", "Antonio", "Danilo", "Fran"];
 export function findMyFriends(friends) {
-  return friends[0] + friends[1] + friends[2];
+  let friendsToNumbers = 0;
+  // need to count how many letters is each index of friends
+  for (let i = 0; i < friends.length; i++) {
+    // check if those letters are <5 then add them as friends
+    if (friends[i].length <= 5) {
+      //count amount of friends and add them as friends
+      friendsToNumbers++;
+    }
+  }
+  return friendsToNumbers;
 }
-findMyFriends(friends);
--- main.test.js
+++ main.test.js
@@ -3,7 +3,7 @@ import { findMyFriends } from "./main";
 
 test("should return 3 friends, given the list of my friends", () => {
   //arrange
-  const friends = ["Juan", "Marcos", "Antonio", "Danilo", "Fran"];
+  const friends = ["Juan", "Marco", "Antonio", "Danilo", "Fran"];
   const expected = 3;
   //act
   const result = findMyFriends(friends);
