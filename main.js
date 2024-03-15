f1c6780b0dfe850ea313b67df3b744ac12be8b82
josetutis <149108908+josetutis@users.noreply.github.com>
2024-03-15 10:04:46 +0000

    checkig that my kata is not the simplest ever made

--- main.js
+++ main.js
@@ -3,4 +3,8 @@
 //A LIST WITH SOME NAMES. I NEED TO BUY A BIRTHDAY CAKE BUT NOT SURE HOW MANY PIECES I NEED.
 //I REMEMBER NONE OF MY FRIEND'S NAMES IS LONGER THAN 5 CHARACTERES.
 
-export function findMyFriends() {}
+const friends = ["Juan", "Marcos", "Antonio", "Danilo", "Fran"];
+export function findMyFriends(friends) {
+  return friends[0] + friends[1] + friends[2];
+}
+findMyFriends(friends);
--- main.test.js
+++ main.test.js
@@ -1,10 +1,12 @@
 import { test, expect } from "vitest";
 import { findMyFriends } from "./main";
 
-test("should return 3 friends, given my array with numbers", () => {
+test("should return 3 friends, given the list of my friends", () => {
   //arrange
   const friends = ["Juan", "Marcos", "Antonio", "Danilo", "Fran"];
   const expected = 3;
+  //act
   const result = findMyFriends(friends);
+  //asert
   expect(result).toBe(expected);
 });
