c7b57960ed9702a36189992f073d4631adf5113a
josetutis <149108908+josetutis@users.noreply.github.com>
2024-03-15 11:21:13 +0000

    ADDED SOME TEST THAT ARE WORKING AS EXPECTED

--- main.test.js
+++ main.test.js
@@ -1,10 +1,39 @@
 import { test, expect } from "vitest";
 import { findMyFriends } from "./main";
 
+test("should return 0 friends, given the list of my friends", () => {
+  //arrange
+  const friends = ["Juanin", "Marcos", "Antonio", "Danilo", "Francisco"];
+  const expected = 0;
+  //act
+  const result = findMyFriends(friends);
+  //asert
+  expect(result).toBe(expected);
+});
+test("should return 0, given the list of my friends", () => {
+  //arrange
+  const friends = [];
+  const expected = 0;
+  //act
+  const result = findMyFriends(friends);
+  //asert
+  expect(result).toBe(expected);
+});
+
 test("should return 3 friends, given the list of my friends", () => {
   //arrange
   const friends = ["Juan", "Marco", "Antonio", "Danilo", "Fran"];
   const expected = 3;
+  //act
+  const result = findMyFriends(friends);
+  //asert
+  expect(result).toBe(expected);
+});
+
+test("should return 2 friends, given the list of my friends", () => {
+  //arrange
+  const friends = ["Juan", "Marcos", "Antonio", "Danilo", "Fran"];
+  const expected = 2;
   //act
   const result = findMyFriends(friends);
   //asert
