3689e401ee37060f98cec6f098fa0192a6ab2f82
josetutis <149108908+josetutis@users.noreply.github.com>
2024-03-15 11:54:10 +0000

    FINAL KATA WITH TESTS AND NO SOLUTIONS

--- PROJECT_PRESENTATION.md
+++ PROJECT_PRESENTATION.md
@@ -1,1 +1,4 @@
 Put your plan, story, and any supporting material here, you could use the help of a readme markdown generator such as [readme.so](https://readme.so/)
+
+![MY PLAN](image.png)
+![All my commits](commitsForMyKata-1.png)
--- main.js
+++ main.js
@@ -4,15 +4,4 @@
 //I REMEMBER NONE OF MY FRIEND'S NAMES HAS NO MORE THAN 4 CHARACTERES
 //IF MY LIST IS EMPTY IT SHOULD RETURN 0 FRIENDS
 //IF ALL THE NAMES IN THE LIST HAS MORE THAN 4 CHARACTERES IT SHOULD RETURN 0
-export function findMyFriends(friends) {
-  let friendsToNumbers = 0;
-  // need to count how many letters is each index of friends
-  for (let i = 0; i < friends.length; i++) {
-    // check if those letters are <5 then add them as friends
-    if (friends[i].length <= 5) {
-      //count amount of friends and add them as friends
-      friendsToNumbers++;
-    }
-  }
-  return friendsToNumbers;
-}
+export function findMyFriends(friends) {}
